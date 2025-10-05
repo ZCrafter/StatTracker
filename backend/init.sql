04853894a8bd6454abafadde710fa4dbaa01e980
ZCrafter <zach510harrison@gmail.com>
2025-10-05 11:45:37 -0400

    Update init.sql

--- backend/init.sql
+++ backend/init.sql
@@ -2,11 +2,11 @@ CREATE TABLE IF NOT EXISTS events (
     id SERIAL PRIMARY KEY,
     event_type VARCHAR(10) NOT NULL,
     location VARCHAR(20) NOT NULL,
-    who VARCHAR(100),
     timestamp TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE IF NOT EXISTS toothbrush_events (
     id SERIAL PRIMARY KEY,
-    timestamp TIMESTAMP DEFAULT NOW()
+    timestamp TIMESTAMP DEFAULT NOW(),
+    used_irrigator BOOLEAN DEFAULT FALSE
 );
